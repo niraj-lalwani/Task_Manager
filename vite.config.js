67b31f163856956792e292fa3c6f125683e55d9f
niraj-lalwani <ascniraj@gmail.com>
2025-06-16 11:58:51 +0530

    added

--- vite.config.js
+++ vite.config.js
@@ -1,68 +1,46 @@
-// vite.config.js
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 import tailwindcss from '@tailwindcss/vite'
-import { VitePWA } from 'vite-plugin-pwa'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
       manifest: {
-        "name": "Task Manager",
-        "short_name": "Tasks",
-        "description": "A simple and efficient task management PWA.",
-        "id": "/",
-        "start_url": "/",
-        "display": "standalone",
-        "background_color": "#ffffff",
-        "theme_color": "#4a90e2",
-        "orientation": "portrait",
-        "icons": [
+        name: 'Task Manager',
+        short_name: 'Vite PWA Project',
+        theme_color: '#ffffff',
+        icons: [
           {
-            "src": "/done.png",
-            "type": "image/png",
-            "sizes": "192x192"
+            src: 'done.png',
+            sizes: '64x64',
+            type: 'image/png'
           },
           {
-            "src": "/done.png",
-            "type": "image/png",
-            "sizes": "512x512"
+            src: 'done.png',
+            sizes: '192x192',
+            type: 'image/png'
           },
           {
-            "src": "/done.png",
-            "type": "image/png",
-            "sizes": "512x512",
-            "purpose": "maskable"
+            src: 'done.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
           },
           {
-            "src": "/done.png",
-            "type": "image/png",
-            "sizes": "512x512",
-            "purpose": "any"
+            src: 'done.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
           }
         ],
-        "launch_handler": {
-          "client_mode": "auto"
-        },
-        "screenshots": [
-          {
-            "src": "/done.png",
-            "sizes": "540x720",
-            "type": "image/png",
-            "label": "Dashboard"
-          },
-          {
-            "src": "/done.png",
-            "sizes": "540x720",
-            "type": "image/png",
-            "label": "Create Task"
-          }
-        ]
       },
-    }),
+    })
   ],
 })
