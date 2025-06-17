05524d5f11ecf97acf0d928383a1df7f47c986f4
niraj-lalwani <ascniraj@gmail.com>
2025-06-17 15:24:55 +0530

    added

---
+++ app/src/main/java/app/vercel/task_manager_umber_nu/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.task_manager_umber_nu.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
