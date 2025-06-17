c4926a3720d46a784be3e1f0cbf5210d6be8c5c4
niraj-lalwani <ascniraj@gmail.com>
2025-06-17 10:09:17 +0530

    added

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import AuthProvider from './context/AuthContext.jsx'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-// import { registerSW } from 'virtual:pwa-register';
-// registerSW();
+import { registerSW } from 'virtual:pwa-register';
+registerSW();
 
 
 createRoot(document.getElementById('root')).render(
